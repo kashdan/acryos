6bab531278497f442ead6042314b3e49df1ea361
kashdan <adamkashtanov@gmail.com>
2015-10-28 00:28:05 -0230

    add research team

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Applied Cryospheric Cons.' });
 });
 
+/* GET team page. */
+router.get('/team', function(req, res, next) {
+  res.render('team', { title: 'Applied Cryospheric Cons.' });
+});
+
 module.exports = router;
